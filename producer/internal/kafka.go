4502874e3c55df74a4ee2fa3b010462646a80cbb
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-10-14 22:59:24 +0700

    added producer email

--- producer/internal/kafka.go
+++ producer/internal/kafka.go
@@ -1,1 +1,48 @@
 package internal
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/plain"
+
+	"github.com/SyaibanAhmadRamadhan/go-pub-sub-kafka/producer/infra"
+)
+
+func KafkaWriter() *kafka.Writer {
+	w := &kafka.Writer{
+		Addr:  kafka.TCP(infra.Get().Application.Kafka.Broker),
+		Topic: infra.Get().Application.Kafka.Topic,
+		Transport: &kafka.Transport{
+			SASL: plain.Mechanism{
+				Username: infra.Get().Application.Kafka.User,
+				Password: infra.Get().Application.Kafka.Pass,
+			},
+		},
+	}
+
+	return w
+}
+
+func WriteMsg(ctx context.Context, mail string, w *kafka.Writer) {
+	msg := map[string]string{
+		"value": "test-value",
+		"to":    mail,
+	}
+
+	kafkaMsg, err := json.Marshal(msg)
+
+	if err != nil {
+		log.Fatal("failed marshal map")
+	}
+
+	if err = w.WriteMessages(ctx, kafka.Message{
+		Value: kafkaMsg,
+	}); err != nil {
+		log.Fatalf("failed write message to kafka | err : %v", err)
+	}
+
+	log.Println("success publish your message | mail : ", mail)
+}
--- producer/main.go
+++ producer/main.go
@@ -1,3 +1,39 @@
-package producer
+package main
 
-func main() {}
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/SyaibanAhmadRamadhan/go-pub-sub-kafka/producer/internal"
+)
+
+func main() {
+	w := internal.KafkaWriter()
+	defer func() {
+		if err := w.Close(); err != nil {
+			log.Printf("failed close kafka writer | err %v", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	for {
+		fmt.Print("Enter email address ('exit' to exit):")
+		scanner.Scan()
+		email := scanner.Text()
+
+		if email == "exit" {
+			break
+		}
+
+		internal.WriteMsg(ctx, email, w)
+
+	}
+
+}
